a7cd732f5ce60de57ad14cf1efa9c29e79258d91
nozymech <sjyang.pt@gmail.com>
2022-07-16 23:07:34 +0800

    add marks to a specific spot

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+let clickNumber = 0 ;
+
 let GameBoard = (function(){
     'use strict';
-    let board = ["X","O","O","O","O","X","X","X","O"];
     let gameBoard = document.getElementById("gameBoard");
-    for (let i = 0; i < board.length; i++) {
-        let x = document.createElement('div');
+    for (let i = 0; i < 9; i++) {
+        let x = document.createElement('button');
         x.className = "square";
         x.id = i;
-        x.innerText = board[i];
+        x.innerText = "" ; 
         gameBoard.append(x);
     };
 })();
+
+const boardSquares = document.querySelectorAll("button.square");
+boardSquares.forEach((button)=>{
+    button.addEventListener('click',()=>{
+        clickNumber = clickNumber +1;
+        console.log(clickNumber);
+        if (clickNumber==1 || clickNumber==3 || clickNumber==5 || clickNumber==7 || clickNumber==9 ){
+            button.innerText= "O";
+        } else if (clickNumber==2 || clickNumber==4 || clickNumber==6 || clickNumber==8) {
+            button.innerText= "X";
+        }
+	})
+})
 
