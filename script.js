c34630a83666ab260c31e408a20e9b139c13a89e
nozymech <sjyang.pt@gmail.com>
2022-07-18 20:29:32 +0800

    Add game logic check win or not

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let gameBoard = document.getElementById("gameBoard");
 let status = document.getElementById("status");
 let square = document.querySelectorAll("button.square")
 let reset = document.getElementById("reset");
+let clickButton = "";
 let clickNumber = 0 ;
 const player = (name,mark,score) => {
   return { name, mark, score };
@@ -33,13 +34,41 @@ boardSquares.forEach((button)=>{
         clickNumber = clickNumber +1;
         if (clickNumber==1 || clickNumber==3 || clickNumber==5 || clickNumber==7 || clickNumber==9 ){
             button.innerText= "O";
-            console.log(button.id);
-            
+            clickButton = Number(button.id);
+            board.splice(clickButton,1,"O");
+            check();
+            console.log(clickButton);
+            console.log(board);
+
             status.innerText= "X turn"
         } else if (clickNumber==2 || clickNumber==4 || clickNumber==6 || clickNumber==8) {
             button.innerText= "X";
-            console.log(button.id);
+            clickButton = Number(button.id);
+            board.splice(clickButton,1,"X")
+            check();
+            console.log(clickButton);
+            console.log(board);
             status.innerText= "O turn"
         }
 	})
 })
+
+let check = () => {
+    if (board[0] != "" && board[0] == board[1] && board[1] == board[2]){
+        board[0] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[3] != "" && board[3] == board[4] && board[4] == board[5]){
+        board[3] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[6]!= ""&& board[8] != "" && board[6] == board[7] && board[7] == board[8]){
+        board[6] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[0] != ""&& board[0] == board[3] && board[3] == board[6]){
+        board[0] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[1] != ""&& board[1] == board[4] && board[4] == board[7]){
+        board[1] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[2] != ""&& board[2] == board[5] && board[5] == board[8]){
+        board[2] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[0] != ""&& board[0] == board[4] && board[4] == board[8]){
+        board[0] == "O" ? console.log("O win") : console.log("X win")
+    }else if (board[2] != ""&& board[2] == board[4] && board[4] == board[6]){
+        board[2] == "O" ? console.log("O win") : console.log("X win")
+    }
+}
